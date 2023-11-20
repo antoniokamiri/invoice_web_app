2cc973370fb75e6aa7f5e1a4bdfe25775107d92e
Antonio Kamiri <53481074+antoniokamiri@users.noreply.github.com>
2023-11-20 18:18:54 +0300

    App

--- invoice_webapp/src/App.jsx
+++ invoice_webapp/src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Header from './components/Header'
+import {Routes, Route, useLocation} from 'react-router-dom';
+import Header from './components/Header';
+import { AnimatePresence } from 'framer-motion';
+import Center from './components/Center';
+import InvoiceInfo from './components/InvoiceInfo';
+import {useDispatch} from 'react-redux';
 
 function App() {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  
   return (
     <div className='dark:bg-[#141625] bg:-[#f8f8fb] duration-300 min-h-screen'>
       {/* header Section */}
       <Header />
-      App
+
+      <AnimatePresence mode='wait'>
+        <Routes location={location} key={location.pathname}>
+
+          <Route element={<Center />} path='' />
+          <Route element={<InvoiceInfo />} path='/invoice' />
+
+        </Routes>
+
+      </AnimatePresence>
     </div>
   )
 }
